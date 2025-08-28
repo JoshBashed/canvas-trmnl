5e18bfd221d37c6a0345bc97b6a7188d6e45c5d2
Josh <36625023+JoshuaBrest@users.noreply.github.com>
2025-08-27 22:07:32 -0700

    feat: support log levels in the logger.

--- src/shared/utilities/loggingUtilities.ts
+++ src/shared/utilities/loggingUtilities.ts
@@ -1,9 +1,19 @@
 export type LogLevel = 'debug' | 'info' | 'warn' | 'error' | 'fatal';
 
+const LOG_LEVEL_MAP = {
+    debug: 0,
+    error: 3,
+    fatal: 4,
+    info: 1,
+    warn: 2,
+} as const satisfies Record<LogLevel, number>;
+
 export class Logger {
     private static mainInstance: Logger = new Logger();
     private topInstance: Logger | undefined;
     private module: string | undefined;
+    private logLevel: LogLevel =
+        process.env.NODE_ENV === 'production' ? 'info' : 'debug';
 
     private constructor() {
         // Private constructor to prevent instantiation
@@ -20,6 +30,11 @@ export class Logger {
         return child;
     }
 
+    public setLogLevel(level: LogLevel): void {
+        if (this.topInstance) this.topInstance.setLogLevel(level);
+        else this.logLevel = level;
+    }
+
     private log(
         level: LogLevel,
         message: string,
@@ -30,6 +45,7 @@ export class Logger {
             this.topInstance.log(level, message, module, ...args);
             return;
         }
+        if (LOG_LEVEL_MAP[level] < LOG_LEVEL_MAP[this.logLevel]) return;
         console.log(
             `%s [%s] ${message}`,
             module ?? 'global',
